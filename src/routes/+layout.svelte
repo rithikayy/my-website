ffe8e2e05e907317fb56de1c2b44710886648cee
rithikayy <rithika1226@gmail.com>
2025-07-18 16:02:15 -0400

    added pic, updated description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
   <img src="/cherry-logo.png" alt="cherry logo" class="h-20" />
 </header>
 
-<main class="text-center font-mono bg-[#faf5ee] text-[#4e2f1c] min-h-screen pt-3">
+<main class="text-center font-mono bg-[#f7efe4] text-[#4e2f1c] min-h-screen pt-3">
   <h1 class="text-xl font-bold my-4">Hi, I'm Rithika!</h1>
   <p class="my-4" style="font-size: 0.925rem;">Welcome to my site, still under construction! 🏗️</p>
   <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,12 @@ function aboutMe() {
     document.getElementById('aboutme')?.scrollIntoView({ behavior: 'smooth' });
   }
 </script>
-<title>Welcome!</title>
 
 <div class="relative text-center p-1">
   <img 
-    src="/main-cherry-website.png" 
+    src="/branch.png" 
     alt="cherry blossom branch" 
-    class="w-120 -mt-30 h-auto ml-100"
+    class="w-120 -mt-23 h-auto ml-111"
   />
   <img 
     src="/flower1.png" 
@@ -35,20 +34,19 @@ function aboutMe() {
 <button class="cursor-pointer inline-flex h-10 items-center justify-center rounded-md text-amber-900 bg-gradient-to-r from-amber-100 via-orange-100 to-orange-200 hover:bg-gradient-to-br focus:ring-0 focus:outline-none focus:ring-rose-200 dark:focus:ring-red-200/50 shadow-lg shadow-rose-300/50 dark:shadow-lg dark:shadow-red-200/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition active:scale-110">Contact</button>
 
 
-<div id="aboutme" class="h-screen flex flex-row items-center justify-center bg-[#faf5ee] px-10 gap-10">
+<div id="aboutme" class="h-screen flex flex-row items-center justify-center bg-[#f7efe4] px-10 gap-10">
   <div class="flex-shrink-0">
     <!-- svelte-ignore a11y_img_redundant_alt -->
-    <img src="/my-pic.jpg" alt="a photo of me!" class="rounded-full w-64 h-64 object-cover dark:focus:ring-red-200/50 shadow-lg shadow-rose-300/50 dark:shadow-lg dark:shadow-red-200/80 transition-transform duration-700 hover:scale-105" />
+    <img src="/mypic1.JPG" alt="a photo of me!" class="rounded-lg max-w-lg h-80 object-cover dark:focus:ring-rose-400/50 shadow-lg shadow-rose-400/50 dark:shadow-lg dark:shadow-rose-400/80 transition-transform duration-700 hover:scale-105" />
   </div>
 
-  <!-- RIGHT: Text -->
   <div class="text-right max-w-md">
     <h1 class="text-3xl font-bold">Nice to meet you! 🌸</h1>
-    <p class="mt-4" style="font-size: 0.925rem;">
-      I'm Rithika, a third-year computer science and statistics student at the University of Toronto.
-      I love working on creative tech projects, helping others learn to code, and building things that are interesting! I'm super excited about data science and machine learning, and I'm working toward a career where I can apply these skills!
+    <p class="mt-4 text-justify max-w-prose ml-auto" style="font-size: 0.925rem;">
+      I'm Rithika, a third-year computer science and statistics student at University of Toronto.
+      I love working on creative tech projects, helping others learn to code, and building things that are interesting! I'm super excited about data science and machine learning, and I'm working toward a career where I can apply these skills.
       <br /><br />
-      Outside of tech, I enjoy watching shows (favourite show rn is <em>Severance</em>!) and biking. Find my contact info down below! :)
+      Outside of tech, I enjoy watching shows and biking. 🌺
     </p>
   </div>
 </div>
